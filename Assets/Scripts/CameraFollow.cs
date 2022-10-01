f40c742a18ce20c45fdf4625b8c1f9c59d22ecb0
Tim <freeman.crazys@gmail.com>
2022-10-01 12:18:58 +0300

    Feature #3. Added cinemachine

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    private CinemachineVirtualCamera _playerPos;
+    void Start() {
+        _playerPos.m_Follow = PlayerLocater.PlayerLocation;
+    }
+    
+}
